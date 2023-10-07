42600c62d2519bfe9957e0e869895a4f1995348f
sunilaws03 <70926307+sunilaws03@users.noreply.github.com>
2023-10-07 05:40:13 -0400

    Update main.tf
    provider "aws" {
        region = "us-east-1"  
    }
    
    resource "aws_instance" "foo" {
      ami           = "ami-05fa00d4c63e32376" # us-west-2
      instance_type = "t2.micro"
      tags = {
          Name = "TF-Instance"
      }
    }

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
- resource "aws_instance" "test" {
-     ami = "ami-036f5574583e16426"
-     availability_zone = "us-east-1"
-     instance_type = "t2.micro"
- }
+provider "aws" {
+    region = "us-east-1"  
+}
+
+resource "aws_instance" "foo" {
+  ami           = "ami-05fa00d4c63e32376" # us-west-2
+  instance_type = "t2.micro"
+  tags = {
+      Name = "TF-Instance"
+  }
+}
