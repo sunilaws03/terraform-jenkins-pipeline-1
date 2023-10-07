ba4e997a3c2628b1c8a92bcf2d16cb7f1000134f
sunilaws03 <70926307+sunilaws03@users.noreply.github.com>
2023-10-07 04:32:46 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
  resource "aws_instance" "test" {
      ami = "ami-036f5574583e16426"
-     availability_zone = "us-east-2"
+     availability_zone = "us-east-1"
      instance_type = "t2.micro"
  }
